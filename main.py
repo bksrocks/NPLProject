f21049e00e8edd91bf21455eb8c0b3dc5e6d8b53
Barbara Szwabowski <37941329+bksrocks@users.noreply.github.com>
2022-04-25 17:39:25 -0400

    Changes on input collection. Increased epoch.

--- main.py
+++ main.py
@@ -89,10 +89,10 @@ def return_best_words(seed_text_input, num_of_words):
 
 plot_graphs(history, 'accuracy')
 
+next_words = int(input("Length of text:"))
 seed_text = input("Start with something: ")
-next_words = 100
 
-for _ in range(input("Length of sentence:")):
+for _ in range(next_words):
     best_three = []  # Assume seed is "I thought that"
     best_words = return_best_words(seed_text, num_of_words=3)  # [today, tomorrow, yesterday]
 
@@ -102,8 +102,8 @@ for _ in range(input("Length of sentence:")):
     seed_text_0_1 = seed_text + " " + best_words[1]  # I thought that tomorrow [b1_0, b1_1, b1_2]
     b1_0, b1_1, b1_2 = return_best_words(seed_text_0_1, num_of_words=3)
 
-    seed_text_0_2 = seed_text + " " + best_words[2]  # I thought that tomorrow [b1_0, b1_1, b1_2]
-    b2_0, b2_1, b2_2 = return_best_words(seed_text_0_1, num_of_words=3)
+    seed_text_0_2 = seed_text + " " + best_words[2]  # I thought that yesterday [b1_0, b1_1, b1_2]
+    b2_0, b2_1, b2_2 = return_best_words(seed_text_0_2, num_of_words=3)
 
     # best_three.append(b0_0)
     # best_three.append(b0_1)
@@ -112,13 +112,13 @@ for _ in range(input("Length of sentence:")):
     print("Option 1:", seed_text_0_0)
     print(f"0.{b0_0}, 1. {b0_1}, 2. {b0_2}")
 
-    print("Option 2:", seed_text_0_0)
+    print("Option 2:", seed_text_0_1)
     print(f"0.{b1_0}, 1. {b1_1}, 2. {b1_2}")
 
-    print("Option 3:", seed_text_0_0)
+    print("Option 3:", seed_text_0_2)
     print(f"0.{b2_0}, 1. {b2_1}, 2. {b2_2}")
 
-    choice = input("Select:")
+    choice = int(input("Select:"))
 
     if choice == 1:
         best_three.append(b0_0)
